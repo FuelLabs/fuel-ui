5e176e0682126aa0d4c8c9f88188bdf659953d3a
Pedro Nauck <pedronauck@gmail.com>
2022-04-29 18:16:30 -0300

    chore(react): add couple storybook addons

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -4,6 +4,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource',
   ],
   framework: '@storybook/react',
 }
