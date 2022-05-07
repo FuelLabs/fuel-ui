036f6def4b3ff611841e2cf1995c12387446b79a
Pedro Nauck <pedronauck@gmail.com>
2022-05-07 16:52:31 -0300

    refactor: eslint, jest and tsup configs

---
+++ packages/config/src/tsup.ts
@@ -0,0 +1,12 @@
+import type { Options } from 'tsup'
+import { defineConfig } from 'tsup'
+
+export function createConfig(options: Options) {
+  return defineConfig({
+    format: ['cjs', 'esm'],
+    splitting: false,
+    sourcemap: true,
+    clean: false,
+    minify: !options.watch,
+  })
+}
--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,5 +1,5 @@
 import { createConfig } from '@fuels/config'
 
 export default createConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.tsx'],
 })
