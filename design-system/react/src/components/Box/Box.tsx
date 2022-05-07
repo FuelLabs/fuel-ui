394b24c1883d52b4e59f771019b2fba2b4cb023e
Pedro Nauck <pedronauck@gmail.com>
2022-05-07 20:02:29 -0300

    feat: add Box component

---
+++ design-system/react/src/components/Box/Box.tsx
@@ -0,0 +1,12 @@
+import { styled } from "@fuel/css";
+import { createElement } from "react";
+
+import type { HTMLProps } from "@/utils";
+import { createComponent } from "@/utils";
+
+export type BoxProps = HTMLProps["div"];
+
+const Root = styled("div");
+export const Box = createComponent<BoxProps>(({ children, ...props }) =>
+  createElement(Root, props, children)
+);
