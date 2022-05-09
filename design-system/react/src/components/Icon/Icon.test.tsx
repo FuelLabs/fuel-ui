6b135dc73eea745bfcb7b784b8a3208b00ed0437
Pedro Nauck <pedronauck@gmail.com>
2022-05-09 18:57:34 -0300

    reactor: accept component as icon prop on Icon

---
+++ design-system/react/src/components/Icon/Icon.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { FiBox } from "react-icons/fi";
+
+import { Icon } from "./Icon";
+
+describe("Icon", () => {
+  it("should render a basic icon component", () => {
+    const { container } = render(<Icon aria-label="Delete" icon="BiTrash" />);
+    expect(container.querySelector("svg")).toBeInTheDocument();
+    expect(screen.getByLabelText("Delete")).toBeInTheDocument();
+  });
+
+  it.only("should render an arbitraty icon as react element", async () => {
+    const { container } = render(<Icon aria-label="Delete" icon={FiBox} />);
+    expect(container.querySelector("svg")).toBeInTheDocument();
+    expect(screen.getByLabelText("Delete")).toBeInTheDocument();
+  });
+});
