ac9ae04f1f442ee78660f9e22e2beee8ab210af3
Pedro Nauck <pedronauck@gmail.com>
2022-05-10 15:23:18 -0300

    fix(config): tsup script warning

--- common/config/tsup.js
+++ common/config/tsup.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const path = require('path');
 
 module.exports = (_options, { withReact } = {}) => ({
-    format: ['cjs', 'esm'],
-    splitting: false,
-    sourcemap: true,
-    clean: false,
-    minify: process.env.NODE_ENV === 'production',
-    ...(withReact && { inject: [path.resolve(__dirname, './react-imports.js')] }),
-  };)
+  format: ['cjs', 'esm'],
+  splitting: false,
+  sourcemap: true,
+  clean: false,
+  minify: process.env.NODE_ENV === 'production',
+  ...(withReact && { inject: [path.resolve(__dirname, './react-imports.js')] }),
+});
