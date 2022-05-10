e61435de880c374ec66030e322f5859bafdcf942
Pedro Nauck <pedronauck@gmail.com>
2022-05-10 15:37:21 -0300

    fix(react): global theme

--- design-system/react/src/styles/GlobalStyles.tsx
+++ design-system/react/src/styles/GlobalStyles.tsx
@@ -1,22 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { colors, globalCss } from "@fuel/css";
+import { colors, darkColors, globalCss, tokens } from "@fuel/css";
+
+import type { FuelTheme } from "..";
+import { useFuelTheme } from "..";
 
 import { opinionated } from "./normalize";
 
 const customStyles = {
   body: {
-    WebkitTapHighlightColor: colors.accent9,
     "-webkit-font-smoothing": "antialiased",
     "-moz-osx-font-smoothing": "grayscale",
+    margin: "0",
+    letterSpacing: 0,
+    fontFamily: tokens.fonts.sans,
+    fontFeatureSettings: '"ss02" on, "ss01" on',
+    textSize: "base" as any,
   },
 };
 
-const styles = (theme: any) => {
-  globalCss(theme, ...opinionated)();
-  globalCss(theme, customStyles)();
+function colorsByTheme(theme: FuelTheme) {
+  return {
+    body: {
+      color: theme === "light" ? colors.textColor : darkColors.textColor,
+      background: theme === "light" ? colors.bodyColor : darkColors.bodyColor,
+    },
+  };
+}
+
+const styles = (theme: FuelTheme) => {
+  globalCss(opinionated)();
+  globalCss(customStyles)();
+  globalCss(colorsByTheme(theme))();
 };
 
-export const GlobalStyles = ({ theme }: any) => {
-  styles(theme);
+export const GlobalStyles = () => {
+  const { theme: fuelTheme } = useFuelTheme();
+  styles(fuelTheme);
   return null;
 };
