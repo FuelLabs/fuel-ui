2d8b7e12ef89563d7af2964cb92a0caad51df7ec
Pedro Nauck <pedronauck@gmail.com>
2022-05-12 16:57:29 -0300

    chore(react): adjust Dialog styles to have same style of Card

--- design-system/react/src/components/Dialog/DialogContent.tsx
+++ design-system/react/src/components/Dialog/DialogContent.tsx
@@ -1,4 +1,4 @@
-import { cx } from "@fuel/css";
+import { cx, styled } from "@fuel/css";
 import * as RDialog from "@radix-ui/react-dialog";
 
 import { createComponent } from "../../utils";
@@ -11,16 +11,24 @@ export type DialogContentProps = RDialog.DialogContentProps & {
   closeClassName?: string;
 };
 
+const Root = styled(RDialog.Content);
 export const DialogContent = createComponent<DialogContentProps>(
-  ({ children, className, overlayClassName, closeClassName, ...props }) => (
+  ({
+    children,
+    className,
+    overlayClassName,
+    closeClassName,
+    css,
+    ...props
+  }) => (
     <RDialog.Portal>
       <RDialog.Overlay className={cx(overlayClassName, CLASSES.Overlay)} />
-      <RDialog.Content {...props} className={cx(className, CLASSES.Content)}>
+      <Root {...props} css={css} className={cx(className, CLASSES.Content)}>
         {children}
         <RDialog.Close className={cx(closeClassName, styles.closeButton())}>
           <Icon icon="BiX" size={20} color="gray10" />
         </RDialog.Close>
-      </RDialog.Content>
+      </Root>
     </RDialog.Portal>
   )
 );
