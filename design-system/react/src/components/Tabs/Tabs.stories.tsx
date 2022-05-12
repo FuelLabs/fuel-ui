fdef07d4f1186b1b2ca33c9fab3f766f168a8f91
Pedro Nauck <pedronauck@gmail.com>
2022-05-12 20:12:34 -0300

    feat(react): add Tabs component

---
+++ design-system/react/src/components/Tabs/Tabs.stories.tsx
@@ -0,0 +1,116 @@
+import { Text } from "..";
+import { Box } from "../Box";
+import { Button } from "../Button";
+import { Card } from "../Card";
+import { Form } from "../Form";
+import { Icon } from "../Icon";
+import { Input } from "../Input";
+import { Stack } from "../Stack";
+
+import type { TabsProps } from "./Tabs";
+import { Tabs } from "./Tabs";
+
+export default {
+  component: Tabs,
+  title: "UI/Tabs",
+  argTypes: {},
+};
+
+function Account() {
+  return (
+    <Card within>
+      <Card.Body>
+        <Text css={{ mt: "$0" }}>
+          Make changes to your account here. Click save when you&apos;re done.
+        </Text>
+        <Stack gap="$4">
+          <Form.Control>
+            <Form.Label htmlFor="name">Name</Form.Label>
+            <Input>
+              <Input.ElementLeft element={<Icon icon="BiUser" />} />
+              <Input.Field
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Type your name"
+              />
+            </Input>
+          </Form.Control>
+          <Form.Control>
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Input>
+              <Input.ElementLeft element={<Icon icon="BiEnvelope" />} />
+              <Input.Field
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Type your email"
+              />
+            </Input>
+          </Form.Control>
+        </Stack>
+      </Card.Body>
+      <Card.Footer direction="row-reverse">
+        <Button size="sm">Save changes</Button>
+      </Card.Footer>
+    </Card>
+  );
+}
+
+function Password() {
+  return (
+    <Card within>
+      <Card.Body>
+        <Text css={{ mt: "$0" }}>
+          Change your password here. After saving, you&apos;ll be logged out.
+        </Text>
+        <Stack gap="$4">
+          <Form.Control>
+            <Form.Label htmlFor="currentPassword">Current password</Form.Label>
+            <Input>
+              <Input.ElementLeft element={<Icon icon="BiLock" />} />
+              <Input.Field
+                type="password"
+                name="currentPassword"
+                id="currentPassword"
+                placeholder="Type your password"
+              />
+            </Input>
+          </Form.Control>
+          <Form.Control>
+            <Form.Label htmlFor="newPassword">New password</Form.Label>
+            <Input>
+              <Input.ElementLeft element={<Icon icon="BiLock" />} />
+              <Input.Field
+                type="password"
+                name="newPassword"
+                id="newPassword"
+                placeholder="Type your password"
+              />
+            </Input>
+          </Form.Control>
+        </Stack>
+      </Card.Body>
+      <Card.Footer direction="row-reverse">
+        <Button size="sm">Save changes</Button>
+      </Card.Footer>
+    </Card>
+  );
+}
+
+export const Usage = (args: TabsProps) => (
+  <Box css={{ maxW: "400px" }}>
+    <Tabs {...args} defaultValue="account">
+      <Tabs.List aria-label="Manage your account">
+        <Tabs.Trigger value="account">Account</Tabs.Trigger>
+        <Tabs.Trigger value="password">Password</Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="account">
+        <Account />
+      </Tabs.Content>
+      <Tabs.Content value="password">
+        <Password />
+      </Tabs.Content>
+    </Tabs>
+  </Box>
+);
---
+++ design-system/react/src/components/Tabs/styles.ts
@@ -0,0 +1,81 @@
+import { css } from '@fuel/css';
+
+const OUTLINE_COLOR = '$gray7';
+const BORDER_RADIUS = '$md';
+const HIGHLIGHT_COLOR = '$accent9';
+
+export const root = css({
+  display: 'flex',
+  flexDirection: 'column',
+  boxShadow: '$sm',
+  borderRadius: BORDER_RADIUS,
+  background: '$gray1',
+});
+
+export const list = css({
+  flexShrink: 0,
+  display: 'flex',
+  borderBottom: `1px solid $borderColor`,
+});
+
+export const trigger = css({
+  all: 'unset',
+  position: 'relative',
+  fontFamily: 'inherit',
+  px: '$5',
+  height: '$12',
+  flex: 1,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textSize: 'base',
+  color: '$textColor',
+  userSelect: 'none',
+  border: '2px solid transparent',
+
+  '&:after': {
+    position: 'absolute',
+    display: 'none',
+    content: '""',
+    bottom: '-3px',
+    left: 0,
+    w: 'calc($full + 4px)',
+    height: '3px',
+    background: 'transparent',
+    transform: 'translateX(-2px)',
+  },
+
+  '&:first-child': {
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
+  '&:last-child': {
+    borderTopRightRadius: BORDER_RADIUS,
+  },
+  '&:hover': {
+    color: HIGHLIGHT_COLOR,
+  },
+  '&[data-state="active"]': {
+    color: HIGHLIGHT_COLOR,
+    borderBottomColor: HIGHLIGHT_COLOR,
+
+    '&:after': {
+      display: 'block',
+      background: HIGHLIGHT_COLOR,
+    },
+  },
+  '&:focus': {
+    borderColor: OUTLINE_COLOR,
+  },
+});
+
+export const content = css({
+  flexGrow: 1,
+  borderBottomLeftRadius: BORDER_RADIUS,
+  borderBottomRightRadius: BORDER_RADIUS,
+  outline: 'none',
+  border: '2px solid transparent',
+
+  '&:focus': {
+    borderColor: OUTLINE_COLOR,
+  },
+});
