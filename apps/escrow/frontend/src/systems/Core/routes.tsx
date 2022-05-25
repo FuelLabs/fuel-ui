0cbc64e1390fdd495cbc9901b5f798cf3b1a0146
Matt Auer <mattauer@umich.edu>
2022-05-25 17:51:22 -0400

    refactor: remove create wallet page

--- apps/escrow/frontend/src/systems/Core/routes.tsx
+++ apps/escrow/frontend/src/systems/Core/routes.tsx
@@ -1,28 +1,11 @@
-import { Navigate, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
-import { RequireWallet } from "./components/RequireWallet";
-import CreateWallet from "./pages/CreateWallet";
 import EscrowPage from "./pages/Escrow";
 import { Pages } from "./types/pages";
 
 export const coreRoutes = (
-  <>
-    <Route
-      path="*"
-      element={
-        <RequireWallet>
-          <Navigate to={Pages.escrow} />
-        </RequireWallet>
-      }
-    />
-    <Route path={Pages.createWallet} element={<CreateWallet />} />
     <Route
       path={Pages.escrow}
-      element={
-        <RequireWallet>
-          <EscrowPage />
-        </RequireWallet>
-      }
+      element={<EscrowPage />}
     />
-  </>
 );
