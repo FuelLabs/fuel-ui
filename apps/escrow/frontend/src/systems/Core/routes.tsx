2ae86f2474bfd20c629d506a2c611c1f39c4d9ae
Matt Auer <mattauer@umich.edu>
2022-05-26 16:20:20 -0400

    fix: routing bug

--- apps/escrow/frontend/src/systems/Core/routes.tsx
+++ apps/escrow/frontend/src/systems/Core/routes.tsx
@@ -4,5 +4,5 @@ import EscrowPage from "./pages/Escrow";
 import { Pages } from "./types/pages";
 
 export const coreRoutes = (
-  <Route path={Pages.escrow} element={<EscrowPage />} />
+  <Route path="*" element={<EscrowPage />} />
 );
