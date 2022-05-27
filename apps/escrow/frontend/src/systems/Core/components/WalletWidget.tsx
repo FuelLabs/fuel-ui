38c94ccd2bb97dbd655b71a34508c0672d42ca41
Matt Auer <mattauer@umich.edu>
2022-05-27 16:18:25 -0400

    feat: display users eth balance

--- apps/escrow/frontend/src/systems/Core/components/WalletWidget.tsx
+++ apps/escrow/frontend/src/systems/Core/components/WalletWidget.tsx
@@ -2,7 +2,9 @@ import { css } from "@fuels-ui/css";
 import { Button } from "@fuels-ui/react";
 import clipboard from "clipboard";
 import { useSetAtom } from "jotai";
-import type { ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { BigNumber } from "fuels";
+import type { CoinQuantity } from "fuels";
 import { FaRegCopy } from "react-icons/fa";
 
 import { useWallet, useWalletList } from "../context/AppContext";
@@ -27,15 +29,28 @@ export const WalletWidget = () => {
   const wallets = useWalletList();
   const wallet = useWallet();
   const setCurrentWalletIndex = useSetAtom(walletIndexAtom);
+  const [balance, setBalance] = useState<CoinQuantity | null>(null);
+
+  // TODO there is probably a much better way to load the balance
+  useEffect(() => {
+    if (!wallet) {
+      return;
+    }
+    loadBalance();
+
+    async function loadBalance() {
+      const nextBalance = await wallet?.getBalances();
+      setBalance(nextBalance![0]);
+    }
+    // TODO fix hardcoding which balance we check
+  }, [wallet]);
 
   const getWalletOptions = () => {
     const walletOptions: Array<JSX.Element> = [];
-    wallets?.forEach(async (nextWallet, i) => {
-      console.log(await nextWallet.getBalances());
+    wallets?.forEach((nextWallet, i) => {
       walletOptions.push(
         <option key={i} value={i}>
-          {nextWallet?.address.slice(0, 4)}...{nextWallet?.address.slice(-4)}
-          ({WALLET_NAMES[i]})
+          {nextWallet?.address.slice(0, 4)}...{nextWallet?.address.slice(-4)} ({WALLET_NAMES[i]})
         </option>
       );
     });
@@ -54,8 +69,10 @@ export const WalletWidget = () => {
     clipboard.copy(wallet!.address);
   };
 
+  // TODO format the eth in a better way and maybe make it generic for different coins
   return (
     <>
+      {balance?.amount.div(1e9).div(1e9).toString()} (ETH)
       <Dropdown className={dropDownStyle()} onChange={handleWalletChange}>
         {getWalletOptions()}
       </Dropdown>
