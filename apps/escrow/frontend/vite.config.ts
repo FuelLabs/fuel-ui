a74c8244fe9e15c4f8a0bf679c8fde407a7084df
Matt Auer <mattauer@umich.edu>
2022-05-31 13:24:25 -0400

    fix: udpate config to be able to compile bigints

--- apps/escrow/frontend/vite.config.ts
+++ apps/escrow/frontend/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: ['es2020'],
+  },
   plugins: [react()],
   define: {
     'process.env': process.env,
