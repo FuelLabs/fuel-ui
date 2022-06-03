91c291ee3e4014223b4cd4613a11d725fa40bcc7
Matt Auer <mattauer@umich.edu>
2022-06-03 17:03:51 -0400

    feat: style balance popup

--- apps/escrow/frontend/src/systems/Core/components/ShowBalances.tsx
+++ apps/escrow/frontend/src/systems/Core/components/ShowBalances.tsx
@@ -1,6 +1,11 @@
-import { Box, Stack } from "@fuels-ui/react";
+import { Box, Heading, Stack } from "@fuels-ui/react";
 import { formatUnits } from "ethers/lib/utils";
 import { useAssets } from "../hooks/useAssets";
+
+import { css } from "@fuels-ui/css";
+import { Card } from "@fuels-ui/react";
+import { CardHeader } from "@fuels-ui/react/src/components/Card/CardHeader";
+import { CardBody } from "@fuels-ui/react/src/components/Card/CardBody";
 
 export const ShowBalances = () => {
     const coins = useAssets();
@@ -11,16 +16,35 @@ export const ShowBalances = () => {
         }
         return "";
     }
- 
+
     return (
-        <Box>
-            <Stack>
-                {coins.map((coin) => (
-                    <div className="mt-4" key={coin.assetId}>
-                        {formatValue(coin.amount)} {coin.symbol}
-                    </div>
-                ))}
-            </Stack>
+        <Box css={{ width: "250px" }}>
+            <Card css={{ bg: "$gray7" }}>
+                <CardHeader>
+                    <Heading>
+                        Balances
+                    </Heading>
+                </CardHeader>
+                <CardBody>
+                    <Stack>
+                        {coins.map((coin) => (
+                            <div className={coinStyle()} key={coin.assetId}>
+                                {formatValue(coin.amount)} {coin.symbol}
+                            </div>
+                        ))}
+                    </Stack>
+                </CardBody>
+            </Card>
         </Box>
-    );    
+    );
 }
+
+const coinStyle = css({
+    bg: "$accent9",
+    color: "$gray1",
+    textSize: "base",
+    font: "$sans",
+    display: "inline-flex",
+    border: "1px solid transparent",
+    borderRadius: "$lg",
+});
