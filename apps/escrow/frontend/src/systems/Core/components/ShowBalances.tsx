a17f4965649bd1d49a539bbdb0bfb34f7f543b39
Matt Auer <mattauer@umich.edu>
2022-06-06 10:56:43 -0400

    fix: style

--- apps/escrow/frontend/src/systems/Core/components/ShowBalances.tsx
+++ apps/escrow/frontend/src/systems/Core/components/ShowBalances.tsx
@@ -1,17 +1,15 @@
-import { Box, Flex, Heading, Stack } from "@fuels-ui/react";
+import { css } from "@fuels-ui/css";
+import { Flex, Heading, Stack, Card } from "@fuels-ui/react";
+import { CardBody } from "@fuels-ui/react/src/components/Card/CardBody";
+import { CardHeader } from "@fuels-ui/react/src/components/Card/CardHeader";
 import { formatUnits } from "ethers/lib/utils";
+
 import { useAssets } from "../hooks/useAssets";
 
-import { css } from "@fuels-ui/css";
-import { Card } from "@fuels-ui/react";
-import { CardHeader } from "@fuels-ui/react/src/components/Card/CardHeader";
-import { CardBody } from "@fuels-ui/react/src/components/Card/CardBody";
 import { DECIMAL_PLACES } from "@/config";
-import { useWallet } from "../context/AppContext";
 
 export const ShowBalances = () => {
-    const wallet = useWallet();
-    let coins = useAssets();
+    const coins = useAssets();
 
     const formatValue = (amount: bigint | null | undefined) => {
         if (amount != null) {
@@ -22,23 +20,25 @@ export const ShowBalances = () => {
 
     return (
         // <Box as="aside" css={{ width: "250px" }}>
-        <Flex css={{ direction: "rtl"}}>
-            <Card css={{ width: "250px", bg: "$gray7" }}>
-                <CardHeader>
-                    <Heading>
-                        Balances
-                    </Heading>
-                </CardHeader>
-                <CardBody>
-                    <Stack>
-                        {coins.map((coin) => (
-                            <div className={coinStyle()} key={coin.assetId}>
-                                {formatValue(coin.amount)} {coin.symbol}
-                            </div>
-                        ))}
-                    </Stack>
-                </CardBody>
-            </Card>
+        <Flex css={{ direction: "rtl" }}>
+            <Flex css={{ direction: "ltr" }}>
+                <Card css={{ width: "250px", bg: "$gray7" }}>
+                    <CardHeader>
+                        <Heading>
+                            Balances
+                        </Heading>
+                    </CardHeader>
+                    <CardBody>
+                        <Stack>
+                            {coins.map((coin) => (
+                                <div className={coinStyle()} key={coin.assetId}>
+                                    {formatValue(coin.amount)} {coin.symbol}
+                                </div>
+                            ))}
+                        </Stack>
+                    </CardBody>
+                </Card>
+            </Flex>
         </Flex>
         // </Box>
     );
