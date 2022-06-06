fe266e14b45361ac9dffce87a5ef94675cb47b47
Matt Auer <mattauer@umich.edu>
2022-06-06 12:08:23 -0400

    fix: multiple workspace error

--- apps/escrow/frontend/scripts/build-contracts.sh
+++ apps/escrow/frontend/scripts/build-contracts.sh
@@ -8,6 +8,6 @@ echo $ESCROW_CONTRACT
 echo "Build Fuel Escrow contract"
 forc build -p $ESCROW_CONTRACT
 echo "Build Types for contracts"
-npx typechain --target fuels --out-dir=./src/systems/Core/types/contracts '../contracts/**/out/debug/**.json'
+pnpm exec typechain --target fuels --out-dir=./src/systems/Core/types/contracts '../contracts/**/out/debug/**.json'
 echo "Prettify code"
-npx prettier --write src/systems/Core/types
+pnpm exec prettier --write src/systems/Core/types
