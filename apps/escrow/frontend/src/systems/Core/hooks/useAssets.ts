b840bbbf00a1d859d1a30b398b08f82859c85130
Matt Auer <mattauer@umich.edu>
2022-06-06 11:24:08 -0400

    fix: make balance display consistent between wallets by sorting by symbol

--- apps/escrow/frontend/src/systems/Core/hooks/useAssets.ts
+++ apps/escrow/frontend/src/systems/Core/hooks/useAssets.ts
@@ -19,5 +19,16 @@ const mergeCoinsWithMetadata = (coins: CoinQuantity[] = []) =>
 export const useAssets = () => {
   const { data: balances } = useBalances();
   const coins = mergeCoinsWithMetadata(balances);
-  return coins;
+  return coins.sort((a, b) => {
+    if (!a.symbol || !b.symbol) {
+      return 0;
+    }
+    if (a.symbol < b.symbol) {
+      return -1;
+    }
+    if (a.symbol > b.symbol) {
+      return 1;
+    }
+    return 0;
+  });
 };
