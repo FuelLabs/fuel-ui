af578d518d07860abb237f3803d6794e2ac5239b
Pedro Nauck <pedronauck@gmail.com>
2022-06-27 21:14:26 -0300

    refactor: github actions

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import baseConfig from '@fuels-ui/test-utils/config';
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  ...baseConfig,
+  rootDir: __dirname,
+  modulePathIgnorePatterns: ['node_modules', 'dist'],
+  reporters: ['default', 'github-actions'],
+  collectCoverageFrom: [
+    '<rootDir>/design-system/react/src/**/*.{ts,tsx}',
+    '!<rootDir>/**/*d.ts',
+    '!<rootDir>/**/*test.{ts,tsx}',
+    '!<rootDir>/**/*stories.{ts,tsx}',
+    '!<rootDir>/**/test-*.{ts}',
+    '!<rootDir>/**/__mocks__/**',
+    '!<rootDir>/**/jest.config.ts',
+    '!<rootDir>/types/**',
+  ],
+};
+
+export default config;
