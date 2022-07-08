a7a290bb30d4768b8bc122417f2c071b031c3fd4
Pedro Nauck <pedronauck@gmail.com>
2022-07-07 23:58:17 -0300

    fix: eslint warnings

--- design-system/react/src/components/Image/Image.stories.tsx
+++ design-system/react/src/components/Image/Image.stories.tsx
@@ -1,12 +1,10 @@
-import type { ImageProps } from './Image'
-import { Image } from './Image'
+import type { ImageProps } from "./Image";
+import { Image } from "./Image";
 
 export default {
   component: Image,
-  title: 'UI/Image',
+  title: "UI/Image",
   argTypes: {},
-}
+};
 
-export const Usage = (args: ImageProps) => (
-  <Image {...args} />
-)
+export const Usage = (args: ImageProps) => <Image {...args} />;
