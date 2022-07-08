ea156bc42a020e7334d215afaeb3c527cf672a26
Pedro Nauck <pedronauck@gmail.com>
2022-07-07 23:56:34 -0300

    feat: generate components index.tsx after generate component

--- design-system/react/scripts/generateComponent.js
+++ design-system/react/scripts/generateComponent.js
@@ -6,13 +6,12 @@ const fs = require('fs-extra');
 const snakeCase = require('lodash/snakeCase');
 const prettier = require('prettier');
 
-(async function () {
-  const argv = yargs(hideBin(process.argv)).argv;
+function generateComponentFiles(argv) {
+  const component = argv.name;
   const templatesDir = path.resolve(process.cwd(), 'templates/component');
   const componentsDir = path.resolve(process.cwd(), 'src/components');
-  const component = argv.name;
   const componentDir = path.join(componentsDir, component);
-  const componentClass = snakeCase(component);
+  const componentClass = snakeCase(component).replaceAll('_', '-');
 
   if (fs.pathExistsSync(componentDir)) {
     throw new Error('This component already exist, choose another name!');
@@ -35,4 +34,22 @@ const prettier = require('prettier');
 
     fs.outputFile(newFilePath, prettier.format(newRawFile, { parser: 'typescript' }));
   }
+}
+
+function generateComponentsIndex() {
+  const componentsDir = path.resolve(process.cwd(), 'src/components');
+  const dirs = fs.readdirSync(componentsDir);
+  const filepath = path.resolve(componentsDir, 'index.tsx');
+  const exportAll = dirs
+    .map((dir) => dir !== 'index.tsx' && `export * from "./${dir}";`)
+    .filter(Boolean);
+
+  fs.outputFile(filepath, prettier.format(exportAll.join('\n'), { parser: 'typescript' }));
+}
+
+(async function () {
+  const argv = yargs(hideBin(process.argv)).argv;
+
+  generateComponentFiles(argv);
+  generateComponentsIndex();
 })();
