6f783358d4226044b2a22ea93b103c052a78737c
Pedro Nauck <pedronauck@gmail.com>
2022-07-11 13:05:48 -0300

    chore: unnescessary changes

--- design-system/react/src/components/ButtonGroup/ButtonGroup.stories.tsx
+++ design-system/react/src/components/ButtonGroup/ButtonGroup.stories.tsx
@@ -13,7 +13,6 @@ export const Usage = (args: ButtonGroupProps) => (
   <ButtonGroup {...args} color="blue" size="sm">
     <Button>First</Button>
     <Button leftIcon="CalendarIcon">Second</Button>
-    <Button leftIcon="CalendarIcon">Second</Button>
     <Button>Third</Button>
   </ButtonGroup>
 );
