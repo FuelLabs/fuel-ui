53fd59c045ad4a496cb9bed3dc3beb16e16c210a
Pedro Nauck <pedronauck@gmail.com>
2022-07-11 13:17:36 -0300

    fix: typo

--- design-system/react/src/components/Accordion/Accordion.stories.tsx
+++ design-system/react/src/components/Accordion/Accordion.stories.tsx
@@ -13,13 +13,13 @@ export const Usage = (args: AccordionProps) => (
   <Box css={{ maxW: "$lg" }}>
     <Accordion {...args} type="single" defaultValue="item-1" collapsible>
       <Accordion.Item value="item-1">
-        <Accordion.Trigger>What&apos; Fuel?</Accordion.Trigger>
+        <Accordion.Trigger>What&apos;s Fuel?</Accordion.Trigger>
         <Accordion.Content>
           The world&apos;s fastest modular execution layer.
         </Accordion.Content>
       </Accordion.Item>
       <Accordion.Item value="item-2">
-        <Accordion.Trigger>Is really fast?</Accordion.Trigger>
+        <Accordion.Trigger>Is it really fast?</Accordion.Trigger>
         <Accordion.Content>Yes, it&apos;s blazingly fast.</Accordion.Content>
       </Accordion.Item>
     </Accordion>
