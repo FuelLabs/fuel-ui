7c8049d30fd6465b8b52baae13230c117951b59f
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-08-30 20:14:22 -0300

    feat: create useAvatarGenerated

--- design-system/react/src/components/Avatar/AvatarGenerated.tsx
+++ design-system/react/src/components/Avatar/AvatarGenerated.tsx
@@ -1,17 +1,10 @@
 import { cx } from "@fuel-ui/css";
-import { toSvg } from "jdenticon";
 
 import { createComponent } from "../../utils";
 import type { BoxProps } from "../Box";
 import { Box } from "../Box";
 
-import { getBackgroundColor } from "./utils";
-
-const SIZES = {
-  sm: 30,
-  md: 40,
-  lg: 50,
-};
+import { useAvatarGenerated } from "./hooks/useAvatarGenerated";
 
 type OmitProps = "children";
 export type AvatarGeneratedProps = BoxProps & {
@@ -26,13 +19,13 @@ export const AvatarGenerated = createComponent<
   unknown,
   OmitProps
 >(({ hash, size = "md", className, css, as, background, ...props }) => {
-  const totalSize = SIZES[size];
-  const backColor = getBackgroundColor(background, hash);
-  const svgString = toSvg(hash, totalSize, {
-    backColor,
-    padding: 0.15,
+  const { svgString, totalSize } = useAvatarGenerated({
+    background,
+    hash,
+    size,
   });
   const classes = cx(className, "fuel_avatar-generated");
+
   return (
     <Box
       {...props}
---
+++ design-system/react/src/components/Avatar/hooks/useAvatarGenerated.tsx
@@ -0,0 +1,50 @@
+import { lightColors } from "@fuel-ui/css";
+import { toSvg } from "jdenticon";
+
+const SIZES = {
+  sm: 30,
+  md: 40,
+  lg: 50,
+};
+
+function getBackgroundColor(
+  backgroundColor?: string,
+  unique?: string
+): string | undefined {
+  if (backgroundColor === "fuel") return lightColors.brand;
+  if (backgroundColor !== "random") return backgroundColor;
+  if (!unique) return unique;
+
+  const hash = [...unique].reduce(
+    (prev, _cur, i) => unique.charCodeAt(i) + ((prev << 5) - prev),
+    0
+  );
+  return [0, 0, 0].reduce(
+    (prev, v, i) =>
+      `${prev}${`00${((hash >> (i * 8)) & 0xff).toString(16)}`.slice(-2)}`,
+    "#"
+  );
+}
+
+type UseAvatarGeneratedProps = {
+  background?: string;
+  hash?: string;
+  size: string;
+};
+
+export function useAvatarGenerated({
+  background,
+  hash,
+  size,
+}: UseAvatarGeneratedProps) {
+  const totalSize = SIZES[size];
+  const backColor = getBackgroundColor(background, hash);
+
+  return {
+    svgString: toSvg(hash, totalSize, {
+      backColor,
+      padding: 0.15,
+    }),
+    totalSize,
+  };
+}
