d8c4be5e0bc42bc05f6ac2dbc422ca4d84bd26f9
Pedro Nauck <pedronauck@gmail.com>
2022-09-05 20:20:18 -0700

    feat(react): add Drawer component

---
+++ design-system/react/src/components/Drawer/Drawer.tsx
@@ -0,0 +1,130 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { DOMAttributes, FocusableElement } from "@react-types/shared";
+import { AnimatePresence } from "framer-motion";
+import type { MutableRefObject } from "react";
+import {
+  useState,
+  useEffect,
+  useRef,
+  Children,
+  createContext,
+  useContext,
+} from "react";
+import type { AriaOverlayProps, OverlayTriggerProps } from "react-aria";
+import { OverlayContainer, useOverlay } from "react-aria";
+import { useOverlayTriggerState } from "react-stately";
+import type { OverlayTriggerState } from "react-stately";
+
+import { createComponent } from "../../utils";
+
+import { DrawerBody } from "./DrawerBody";
+import { DrawerClose } from "./DrawerClose";
+import { DrawerContent } from "./DrawerContent";
+import { DrawerTrigger } from "./DrawerTrigger";
+
+// ----------------------------------------------------------------------------
+// Context
+// ----------------------------------------------------------------------------
+
+type DrawerSide = "right" | "left";
+type DrawerSize = "sm" | "md" | "lg";
+
+export type DrawerContext = {
+  ref: React.MutableRefObject<HTMLDivElement | null>;
+  state?: OverlayTriggerState;
+  overlayProps: DOMAttributes<FocusableElement>;
+  underlayProps: DOMAttributes<FocusableElement>;
+  side: DrawerSide;
+  size: DrawerSize | string | number;
+  shouldCloseOnClickAway?: boolean;
+};
+
+const ctx = createContext<DrawerContext>({} as DrawerContext);
+
+export function useDrawer() {
+  return useContext(ctx);
+}
+
+// ----------------------------------------------------------------------------
+// Component
+// ----------------------------------------------------------------------------
+
+export type DrawerProps = OverlayTriggerProps &
+  Omit<AriaOverlayProps, "shouldCloseOnInteractOutside"> & {
+    containerRef?: MutableRefObject<any>;
+    side?: DrawerSide;
+    size: DrawerSize | string | number;
+    shouldCloseOnClickAway?: boolean;
+  };
+
+type OmitProps = "as" | "className" | "css";
+type ElementType = "div";
+type ObjProps = {
+  Body: typeof DrawerBody;
+  CloseButton: typeof DrawerClose;
+  Content: typeof DrawerContent;
+  Trigger: typeof DrawerTrigger;
+};
+
+export const Drawer = createComponent<
+  DrawerProps,
+  ObjProps,
+  OmitProps,
+  ElementType
+>(
+  ({
+    side = "right",
+    size = "sm",
+    shouldCloseOnClickAway = true,
+    containerRef,
+    children,
+    ...opts
+  }) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const [container, setContainer] = useState<HTMLElement | null>(null);
+    const state = useOverlayTriggerState(opts);
+    const { overlayProps, underlayProps } = useOverlay(
+      { ...opts, isOpen: state.isOpen, onClose: state.close },
+      ref
+    );
+
+    const ctxProps = {
+      ref,
+      side,
+      size,
+      state,
+      overlayProps,
+      underlayProps,
+      shouldCloseOnClickAway,
+    };
+
+    const customChildren = Children.toArray(children).map((child: any) => {
+      if (child?.type.id === "DrawerContent") {
+        return (
+          <OverlayContainer
+            key={child?.type.id}
+            {...(container && { portalContainer: container })}
+          >
+            <AnimatePresence key={child.type.id}>
+              {state.isOpen && <>{child}</>}
+            </AnimatePresence>
+          </OverlayContainer>
+        );
+      }
+      return child;
+    });
+
+    useEffect(() => {
+      if (containerRef?.current) {
+        setContainer(containerRef.current);
+      }
+    }, [containerRef?.current]);
+
+    return <ctx.Provider value={ctxProps}>{customChildren}</ctx.Provider>;
+  }
+);
+
+Drawer.Body = DrawerBody;
+Drawer.CloseButton = DrawerClose;
+Drawer.Content = DrawerContent;
+Drawer.Trigger = DrawerTrigger;
---
+++ design-system/react/src/components/Drawer/styles.ts
@@ -0,0 +1,57 @@
+import { css } from '@fuel-ui/css';
+
+export const overlay = css({
+  position: 'absolute',
+  zIndex: '$50',
+  inset: 0,
+  background: 'rgba(0, 0, 0, 0.5)',
+  display: 'flex',
+
+  variants: {
+    side: {
+      left: {
+        justifyContent: 'flex-start',
+      },
+      right: {
+        justifyContent: 'flex-end',
+      },
+    },
+  },
+
+  defaultVariants: {
+    side: 'right',
+  },
+});
+
+export const content = css({
+  overflowX: 'auto',
+  position: 'relative',
+  minHeight: '100%',
+  maxHeight: '100%',
+  background: '$gray1',
+  boxShadow: '$lg',
+});
+
+export const close = css({
+  position: 'absolute',
+  top: '$2',
+  right: '$2',
+  padding: '$0 !important',
+});
+
+// ----------------------------------------------------------------------------
+// Sizes
+// ----------------------------------------------------------------------------
+
+type Size = string | number;
+
+const SIZES = {
+  sm: '300px',
+  md: '500px',
+  lg: '768px',
+};
+
+export function getSize(size: Size) {
+  const width = SIZES[size] || size;
+  return { maxWidth: width, width };
+}
