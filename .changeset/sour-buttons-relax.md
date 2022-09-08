8f424da0d0d6f34fd5f01d0f72b26966607d3239
Pedro Nauck <pedronauck@gmail.com>
2022-09-08 10:03:29 -0700

    fix(react): dropdown story (#138)

---
+++ .changeset/sour-buttons-relax.md
@@ -0,0 +1,5 @@
+---
+"@fuel-ui/react": patch
+---
+
+Fix: Dropdown width on dropdown story.
--- design-system/react/src/components/Dropdown/Dropdown.stories.tsx
+++ design-system/react/src/components/Dropdown/Dropdown.stories.tsx
@@ -19,7 +19,7 @@ const Content = (props: Partial<DropdownProps>) => {
   return (
     <Dropdown {...props}>
       <Dropdown.Trigger>
-        <Button css={{ width: "300px" }}>Click here</Button>
+        <Button>Click here</Button>
       </Dropdown.Trigger>
       <Dropdown.Menu autoFocus disabledKeys={["edit"]} aria-label="Actions">
         <Dropdown.MenuItem key="settings" textValue="Settings">
