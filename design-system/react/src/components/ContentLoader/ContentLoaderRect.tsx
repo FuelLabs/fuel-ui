43599295f992a4a4646d0d18750592a9f5c8faec
luizstacio <luizstacio@gmail.com>
2022-12-05 17:23:25 -0800

    refact: rename method getCSSValue for toCSSValue

--- design-system/react/src/components/ContentLoader/ContentLoaderRect.tsx
+++ design-system/react/src/components/ContentLoader/ContentLoaderRect.tsx
@@ -1,4 +1,4 @@
-import { getCSSValue } from '~/utils/css';
+import { toCSSValue } from '~/utils/css';
 
 export function Rect({
   x,
@@ -10,15 +10,15 @@ export function Rect({
   stickX?: 'right' | 'left';
   stickY?: 'top' | 'bottom';
 }) {
-  const xValue = getCSSValue(x);
-  const yValue = getCSSValue(y);
+  const xValue = toCSSValue(x);
+  const yValue = toCSSValue(y);
   let finalX = x;
   let finalY = y;
   if (stickX === 'right') {
-    finalX = `calc(100% - ${xValue} - ${getCSSValue(props.width)})`;
+    finalX = `calc(100% - ${xValue} - ${toCSSValue(props.width)})`;
   }
   if (stickY === 'bottom') {
-    finalY = `calc(100% - ${yValue} - ${getCSSValue(props.height)})`;
+    finalY = `calc(100% - ${yValue} - ${toCSSValue(props.height)})`;
   }
   return <rect x={finalX} y={finalY} {...props} />;
 }
