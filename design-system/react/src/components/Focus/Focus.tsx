359e54b26886d6b2a9b815ac17f0d0f800e7e7de
Pedro Nauck <pedronauck@gmail.com>
2023-01-30 21:10:25 -0300

    fix(react): ref for aria focus

--- design-system/react/src/components/Focus/Focus.tsx
+++ design-system/react/src/components/Focus/Focus.tsx
@@ -1,6 +1,9 @@
 import { FocusArrowNavigator } from './FocusArrowNavigator';
 import { FocusScope } from './FocusScope';
 
+export { FocusArrowNavigatorProps } from './FocusArrowNavigator';
+export { FocusScopeProps } from './FocusScope';
+
 export const Focus = {
   Scope: FocusScope,
   ArrowNavigator: FocusArrowNavigator,
