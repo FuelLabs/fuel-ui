09acdfbe8333215a2a37423aa1cbba9ff7ec31a1
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-02-01 19:23:34 -0300

    fix: testing

--- design-system/react/src/components/PasswordStrength/utils/passwordStrength.test.ts
+++ design-system/react/src/components/PasswordStrength/utils/passwordStrength.test.ts
@@ -11,39 +11,59 @@ const commonPassword = 'qwe123';
 
 describe('passwordChecker', () => {
   it('should validate a strong password correctly', () => {
-    const { casingChecker, lengthChecker, symbolsAndDigitsChecker } =
-      passwordChecker(strongPassword);
+    const {
+      casingChecker,
+      lengthChecker,
+      symbolsAndDigitsChecker,
+      commonChecker,
+    } = passwordChecker(strongPassword);
 
     expect(casingChecker).toBeTruthy();
     expect(lengthChecker).toBeTruthy();
     expect(symbolsAndDigitsChecker).toBeTruthy();
+    expect(commonChecker).toBeTruthy();
   });
 
   it('should validate an average password with lowercase/uppercase and symbols/digits correctly', () => {
-    const { casingChecker, lengthChecker, symbolsAndDigitsChecker } =
-      passwordChecker(averageCharactersAndSymbolsPassword);
+    const {
+      casingChecker,
+      lengthChecker,
+      symbolsAndDigitsChecker,
+      commonChecker,
+    } = passwordChecker(averageCharactersAndSymbolsPassword);
 
     expect(casingChecker).toBeTruthy();
     expect(lengthChecker).toBeFalsy();
     expect(symbolsAndDigitsChecker).toBeTruthy();
+    expect(commonChecker).toBeTruthy();
   });
 
   it('should validate an average password with lowercase/uppercase and 6+ digits correctly', () => {
-    const { casingChecker, lengthChecker, symbolsAndDigitsChecker } =
-      passwordChecker(averageCharactersAndSixDigitsPassword);
+    const {
+      casingChecker,
+      lengthChecker,
+      symbolsAndDigitsChecker,
+      commonChecker,
+    } = passwordChecker(averageCharactersAndSixDigitsPassword);
 
     expect(casingChecker).toBeTruthy();
     expect(lengthChecker).toBeTruthy();
     expect(symbolsAndDigitsChecker).toBeFalsy();
+    expect(commonChecker).toBeTruthy();
   });
 
   it('should validate a weak password correctly', () => {
-    const { casingChecker, lengthChecker, symbolsAndDigitsChecker } =
-      passwordChecker(weakPassword);
+    const {
+      casingChecker,
+      lengthChecker,
+      symbolsAndDigitsChecker,
+      commonChecker,
+    } = passwordChecker(weakPassword);
 
     expect(casingChecker).toBeFalsy();
     expect(lengthChecker).toBeTruthy();
     expect(symbolsAndDigitsChecker).toBeFalsy();
+    expect(commonChecker).toBeFalsy();
   });
 
   it('should validate a common password correctly', () => {
@@ -52,21 +72,26 @@ describe('passwordChecker', () => {
       lengthChecker,
       symbolsAndDigitsChecker,
       commonChecker,
-    } = passwordChecker(commonPassword, 8);
+    } = passwordChecker(commonPassword);
 
     expect(casingChecker).toBeFalsy();
-    expect(lengthChecker).toBeFalsy();
+    expect(lengthChecker).toBeTruthy();
     expect(symbolsAndDigitsChecker).toBeFalsy();
-    expect(commonChecker).toBeTruthy();
+    expect(commonChecker).toBeFalsy();
   });
 
   it('should be falsy for every rule if minimun length is 8', () => {
-    const { casingChecker, lengthChecker, symbolsAndDigitsChecker } =
-      passwordChecker(weakPassword, 8);
+    const {
+      casingChecker,
+      lengthChecker,
+      symbolsAndDigitsChecker,
+      commonChecker,
+    } = passwordChecker(weakPassword, 8);
 
     expect(casingChecker).toBeFalsy();
     expect(lengthChecker).toBeFalsy();
     expect(symbolsAndDigitsChecker).toBeFalsy();
+    expect(commonChecker).toBeFalsy();
   });
 });
 
