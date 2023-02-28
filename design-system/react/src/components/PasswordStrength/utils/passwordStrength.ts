b24aba133c16449e2a24ee3b30a85bae558fa055
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-02-28 18:52:52 -0300

    chore

--- design-system/react/src/components/PasswordStrength/utils/passwordStrength.ts
+++ design-system/react/src/components/PasswordStrength/utils/passwordStrength.ts
@@ -6,8 +6,6 @@ export const passwordChecker = (password: string, minLength: number = 6) => {
   const symbolsAndDigitsChecker =
     /(?=.*[\d])\w+/g.exec(password) !== null &&
     /(?=.*[@$!%#?&*+\\\-/_^()=[\]{}|'";:<>,.])\w+/g.exec(password) !== null;
-
-  // ^() _ =[]{}\|<>;:,.
   const casingChecker =
     /(?=.*[a-z])\w+/g.exec(password) !== null &&
     /(?=.*[A-Z])\w+/g.exec(password) !== null;
