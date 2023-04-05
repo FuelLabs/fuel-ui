496ce36787542e36f0165a4e59e1465af02a69e1
Pedro Nauck <pedronauck@gmail.com>
2023-04-05 15:31:32 -0300

    fix: build libs

--- stitches/core/tsup.config.js
+++ stitches/core/tsup.config.js
@@ -1,7 +1,10 @@
+/* eslint-disable import/no-relative-packages */
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'tsup';
+
 import baseConfig from '../../common/config/tsup';
 
-export default defineConfig(() => ({
+export default defineConfig((options) => ({
   ...baseConfig(options, { withReact: false }),
   format: ['esm', 'cjs'],
   entry: ['src/index.js'],
