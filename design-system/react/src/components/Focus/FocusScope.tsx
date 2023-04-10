3df99a581b450bf3a539d57e7900f60ccb7833b7
Pedro Nauck <pedronauck@gmail.com>
2023-04-10 16:51:16 -0300

    fix: typing imports

--- design-system/react/src/components/Focus/FocusScope.tsx
+++ design-system/react/src/components/Focus/FocusScope.tsx
@@ -1,3 +1,2 @@
 /// <reference types="@react-aria/focus" />
-export type { FocusScopeProps } from 'react-aria';
-export { FocusScope } from 'react-aria';
+export { FocusScope, FocusScopeProps } from 'react-aria';
