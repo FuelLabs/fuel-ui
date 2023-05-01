c2bd2e9368f8078d38469aeb9ab417981c98b683
Pedro Nauck <pedronauck@gmail.com>
2023-05-01 16:25:14 -0300

    fix: generated changeset

--- scripts/changesets/next.mjs
+++ scripts/changesets/next.mjs
@@ -1,4 +1,4 @@
 import { writeFile } from 'node:fs/promises';
 
-const output = `---\n"fuels": patch\n---\n\nincremental\n`;
+const output = `---\n"@fuel-ui/react": patch\n---\n\nincremental\n`;
 writeFile('.changeset/fuel-labs-ci.md', output);
