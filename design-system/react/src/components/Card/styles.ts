e0a04e542f1f4d21084042b8800f4129333739d6
Pedro Nauck <pedronauck@gmail.com>
2023-05-22 21:59:16 -0300

    chore: add border on cards

--- design-system/react/src/components/Card/styles.ts
+++ design-system/react/src/components/Card/styles.ts
@@ -2,13 +2,11 @@ import { css } from '@fuel-ui/css';
 
 export const card = css({
   layer: 'layer-card',
-  p: '$7',
 });
 
 export const header = css({
-  pb: '$5',
-  mb: '$5',
-  borderBottom: '1px solid $border',
+  px: '$5',
+  pt: '$5',
 
   '& .fuel_Heading': {
     margin: 0,
@@ -17,10 +15,11 @@ export const header = css({
 
 export const body = css({
   display: 'block',
+  px: '$5',
+  py: '$5',
 });
 
 export const footer = css({
-  mt: '$5',
-  pt: '$5',
-  borderTop: '1px solid $border',
+  px: '$5',
+  pb: '$5',
 });
