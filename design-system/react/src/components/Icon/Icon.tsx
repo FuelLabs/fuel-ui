629e24a36d5cc11701e2512310f1c7c7e14b8adc
Pedro Nauck <pedronauck@gmail.com>
2023-07-30 01:11:56 -0300

    feat: add new @fuel-ui/icons package

--- design-system/react/src/components/Icon/Icon.tsx
+++ design-system/react/src/components/Icon/Icon.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { cx } from '@fuel-ui/css';
+import sprite from '@fuel-ui/icons/sprite.svg';
 import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
-import * as TablerIcons from '@tabler/icons-react';
 import type { ReactElement } from 'react';
-import { createElement, useMemo, cloneElement } from 'react';
+import { Fragment, cloneElement, createElement } from 'react';
 
-import { _unstable_createComponent } from '../../utils';
+import { _unstable_createComponent, omit } from '../../utils';
 
 import type * as t from './defs';
 
@@ -13,48 +14,39 @@ import { createStyle, useElementProps, useStyles } from '~/hooks';
 
 export const Icon = _unstable_createComponent<t.IconDef>(
   Components.Icon,
-  ({
-    as = 'i',
-    label: initialLabel,
-    inline,
-    icon,
-    color,
-    className,
-    wrapperClassName,
-    css,
-    size = 18,
-    stroke = 2,
-    ...props
-  }) => {
+  (props) => {
+    const {
+      as = 'i',
+      label: initialLabel,
+      inline,
+      icon,
+      color,
+      className,
+      wrapperClassName,
+      css,
+      size = 18,
+      stroke = 1.5,
+      svgProps,
+      ...rest
+    } = props;
+
     const iconProps = {
+      'aria-hidden': true,
       className: cx('fuel_Icon', `fuel_Icon-${icon}`, className),
       focusable: false,
-      'aria-hidden': true,
-      size,
-      stroke,
+      strokeWidth: String(stroke),
     };
 
-    const iconElement = useMemo<ReactElement>(
-      (() => {
-        if (typeof icon === 'string') {
-          const Component = TablerIcons[`Icon${icon}`];
-          if (!Component) {
-            throw new Error(`Icon ${icon} not found`);
-          }
-          return <Component />;
-        }
-        return icon;
-      }) as () => ReactElement,
-      [icon]
-    );
-
-    let label = initialLabel || props['aria-label'];
+    let label = initialLabel || rest['aria-label'];
     if (!label && typeof icon === 'string') {
       label = `Icon ${icon}`;
     }
+    if (!label && (icon as ReactElement)?.props?.icon) {
+      label = `Icon ${(icon as ReactElement).props?.icon}`;
+    }
 
     const classes = useStyles(styles, {
-      ...props,
+      ...rest,
       css: {
         display: inline ? 'inline-flex' : 'flex',
         ...(color && { color: `$${color}` }),
@@ -62,30 +54,51 @@ export const Icon = _unstable_createComponent<t.IconDef>(
       },
     });
 
-    const elementProps = useElementProps(props, classes.root, {
-      'aria-label': label || '',
+    const elementProps = useElementProps(rest, classes.root, {
+      'aria-label': label,
       className: wrapperClassName,
     });
 
+    function renderElement() {
+      if (typeof icon === 'string') {
+        return (
+          <>
+            <svg
+              fill="transparent"
+              {...iconProps}
+              {...svgProps}
+              width={size}
+              height={size}
+            >
+              <use href={`${sprite}#${icon}`} />
+            </svg>
+            <VisuallyHidden.Root>{label || icon}</VisuallyHidden.Root>
+          </>
+        );
+      }
+
+      return cloneElement(icon as any, {
+        ...omit(['icon'], props),
+      });
+    }
+
     return createElement(
-      as,
+      typeof icon === 'string' ? Fragment : as,
       elementProps,
-      <>
-        {cloneElement(iconElement, iconProps)}
-        <VisuallyHidden.Root>{label || icon}</VisuallyHidden.Root>
-      </>
+      renderElement()
     );
   }
 );
 
-const iconList = Object.keys(TablerIcons) as t.Icons[];
-
 Icon.id = 'Icon';
-Icon.iconList = iconList;
 Icon.is = (icon: t.Icons) => icon;
 
 const styles = createStyle(Components.Icon, {
   root: {
     is: ['centered'],
+
+    '&, & *': {
+      bg: 'transparent !important',
+    },
   },
 });
