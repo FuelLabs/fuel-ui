415a1df078d5f5dec0bf3b87d8f3aa584d6b0512
Pedro Nauck <pedronauck@gmail.com>
2023-08-01 16:34:19 -0300

    fix: eslint warn

--- design-system/react/src/utils/misc.ts
+++ design-system/react/src/utils/misc.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
-
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const noop = () => {};
 
